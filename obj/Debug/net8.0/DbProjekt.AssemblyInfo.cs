a09f7b7c531232f7f8975145d17d217a2d43abd9
notvivi <tomanova@spsejecna.cz>
2026-01-11 18:53:38 +0100

    edited database export

--- obj/Debug/net8.0/DbProjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/DbProjekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbProjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6759af7b929d6ade49440d294e2ff081e07e9327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690b77b86458dffedb27b5b1f890a1a971d8b721")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbProjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbProjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
